4dcfc9b56fde18d9b9b25765cfb9e6d87790933c
ArseniiTi <tiurin@sheridancollege.ca>
2020-04-01 22:43:15 -0400

    Final

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,7 +7,7 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-
+// GUI calculator Sheridan - Java 2 final project
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
